b4fe30d2449a4e748214e418af03927e8eb4490b
如果没有你 <1085160569@qq.com>
2019-08-24 15:41:55 +0800

    Revert "Revert "Merge branch 'dev'""
    This reverts commit 05f902fad1640e4656be95931e7aed768bc68aeb.

--- src/main/java/com/example/apollodynamicdatasource/ApolloDynamicDatasourceApplication.java
+++ src/main/java/com/example/apollodynamicdatasource/ApolloDynamicDatasourceApplication.java
@@ -4,12 +4,14 @@ import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableApolloConfig
 @SpringBootApplication
 @MapperScan("com.example.apollodynamicdatasource.*.mapper")
 @EnableTransactionManagement
+@EnableConfigurationProperties
 public class ApolloDynamicDatasourceApplication {
 
     public static void main(String[] args) {
